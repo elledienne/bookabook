a35a3e6445ec3bb99f8d128f90c45c2c3d277773
Lorenzo De Nobili <elledienne@gmail.com>
2016-03-07 19:40:35 +0100

    Add app file, basic routing

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+angular.module('bookalion', [
+  'ui.router'
+])
+.config(['$stateProvider', '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    
+    $stateProvider  
+      // route to show form
+      .state('form', {
+          url: '/form',
+          templateUrl: 'assets/templates/form.html',
+          controller: 'formController'
+      })
+      
+      .state('form.date-zip', {
+          url: '/date-zip',
+          templateUrl: 'assets/templates/form-date-zip.html'
+      })
+      
+      .state('form.cleaning-details', {
+          url: '/cleaning-details',
+          templateUrl: 'assets/templates/form-cleaning-details.html'
+      });
+
+    // default
+    $urlRouterProvider.otherwise('/form/date-zip');
+
+}]);

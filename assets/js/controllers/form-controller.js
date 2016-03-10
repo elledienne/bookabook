1b5ece5977c83955daca9d5641c92783f538a1a9
Lorenzo De Nobili <elledienne@gmail.com>
2016-03-10 04:25:25 +0100

    Moved some logic (data) to formFactory

--- assets/js/controllers/form-controller.js
+++ assets/js/controllers/form-controller.js
@@ -1,69 +1,10 @@
-angular.module('bookalion')
-.controller('formController', function ($scope, $state) {
+angular.module('bookalion.controller.formcontroller', []).
+controller('formController', ['$scope', '$state', 'FormFactory', function ($scope, $state, FormFactory) {
 
-  $scope.formSetup = {
-    times: [],
-    durations: [
-      {h:'2.5', desc:'Quick cleaning'},
-      {h:'3', desc: null},
-      {h:'3,5', desc:'Mid-sized flat'},
-      {h:'4', desc: null},
-      {h:'4.5', desc: null},
-      {h:'5', desc: null},
-      {h:'5.5', desc:'Big flat'},
-      {h:'6', desc: null},
-      {h:'7', desc: null},
-      {h:'8', desc: null}
-    ],
-    frequencies: [
-      {freq: 'once', best: false, price: 'from 14,90 €/h'},
-      {freq: 'weekly', best: false, price: 'from 13,90 €/h'},
-      {freq: 'every 2 weeks', best: true, price: 'from 13,90 €/h'},
-      {freq: 'every 4 weeks', best: false, price: 'from 13,90 €/h'}
-    ],
-    pickers: {
-      time: false,
-      duration: false,
-      frequency: false
-    },
-    nav: {
-      next: ['cleaning-details'],
-      curr: ['date-zip'],
-      prev: []
-    }
-  };
-
-  $scope.formData = {
-    date: null,
-    time: {
-      value: null,
-      isFlexible: false,
-    },
-    zip: null,
-    duration: null,
-    frequency: null
-  };
-
+  $scope.formSetup = FormFactory.formSetup;
+  $scope.formData = FormFactory.formData;
   $scope.navHandler = [true];
-
-  var populateTimes = function () {
-    var startTime = 6;
-    var endTime = 23;
-    var minutesOptions = ['00', '30'];
-
-    for (startTime; startTime <= endTime; startTime++) {
-      for (var counter = 0; counter < 2; counter++) {
-        var hour = startTime.toString().length === 1 ? '0' + startTime : startTime;
-        var minutes = counter === 0 ? '00' : '30';
-
-        $scope.formSetup.times.push(hour + ':' + minutes);
-      }
-    }
-  }();
-
-  var showData = function () {
-    alert(JSON.stringify($scope.formData, null, 2))
-  }
+  $scope.formatDuration = FormFactory.formatDuration;
 
   var validate = {
     'date-zip': function () {
@@ -74,7 +15,8 @@ angular.module('bookalion')
       return date && time && zip;
     },
     'cleaning-details': function () {
-
+      // as for date-zip
+      // [...] 
     }
   };
 
@@ -87,7 +29,6 @@ angular.module('bookalion')
   };
 
   $scope.togglePicker = function (popupName) {
-    // console.log(popupName, !$scope.formSetup.pickers[popupName])
     if (!$scope.formSetup.pickers[popupName]) {
       for (picker in $scope.formSetup.pickers) {
         $scope.formSetup.pickers[picker] = false;
@@ -97,7 +38,6 @@ angular.module('bookalion')
   };
 
   $scope.goBack = function () {
-    console.log($scope.formSetup.nav.prev)
     if ($scope.formSetup.nav.prev.length) {
       var prev = $scope.formSetup.nav.prev.pop();
       $scope.formSetup.nav.next.push($scope.formSetup.nav.curr.pop());
@@ -108,7 +48,6 @@ angular.module('bookalion')
   };
 
   $scope.goNext = function () {
-
     if ($scope.formSetup.nav.next.length) {
       if (!validate[$scope.formSetup.nav.curr[0]]()){
         return;
@@ -120,17 +59,8 @@ angular.module('bookalion')
       $scope.navHandler.push(true);
       $state.go('^.' + next);
     } else {
-      showData();
+      FormFactory.showData($scope.formData);
     }
   }
 
-  $scope.formatDuration = function () {
-    var h = $scope.formData.duration;
-    if (h) {
-      h += 'h';
-    }
-    return h;
-  }
-
-
-});
+}]);
